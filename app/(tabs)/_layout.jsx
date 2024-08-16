026840c9ed3ed1c816907ed7ccdb365eb9e4b535
belalelmi <belalelmi@gmail.com>
2024-08-15 20:36:45 -0400

    Set up file folder structure and started bottom tabs

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,93 @@
+import { View, Text, Image } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+
+import { icons } from '../../constants';
+
+
+const TabIcon = ({icon, color, name , focused}) => {
+  return (
+    <View className="items-center justify-center gap-2">
+    <Image 
+      source={icon}
+      tintColor={color}
+      className="w-6 h-6"
+    />
+    <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+    {name}
+    </Text>
+    </View>
+  )
+}
+
+const TabsLayout = () => {
+  return (
+  <>
+  <Tabs 
+  screenOptions={{
+    tabBarShowLabel: false
+  }}
+  >
+    <Tabs.Screen
+    name="profile"
+    options={{
+      title: 'Profile',
+      headerShown: false,
+      tabBarIcon: ({color, focused}) => (
+        <TabIcon 
+        icon={icons.profile}
+        name='Profile'
+        focused={focused}
+        color={color}
+        />
+      )
+    }}
+    />
+    <Tabs.Screen
+    name="bookmark"
+    options={{
+      title: 'Bookmark',
+      headerShown: false,
+      tabBarIcon: ({color, focused}) => (
+        <TabIcon 
+        icon={icons.bookmark}
+        name='Bookmark'
+        focused={focused}
+        color={color}
+        />
+      )
+    }}
+    /><Tabs.Screen
+    name="create"
+    options={{
+      title: 'Create',
+      headerShown: false,
+      tabBarIcon: ({color, focused}) => (
+        <TabIcon 
+        icon={icons.plus}
+        name='Create'
+        focused={focused}
+        color={color}
+        />
+      )
+    }}
+    /><Tabs.Screen
+    name="home"
+    options={{
+      title: 'Home',
+      headerShown: false,
+      tabBarIcon: ({color, focused}) => (
+        <TabIcon 
+        icon={icons.home}
+        name='Home'
+        focused={focused}
+        color={color}
+        />
+      )
+    }}
+    />
+  </Tabs>
+  </>
+  )
+}
+
+export default TabsLayout
